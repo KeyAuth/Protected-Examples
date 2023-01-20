d1164b0256659932270629a865e24afa07dd706d
ghosted <120521315+ghosted-oce@users.noreply.github.com>
2023-01-19 22:08:21 -0500

    pushed antidebug & hide thread code

--- C++/protection.h
+++ C++/protection.h
@@ -1,1 +1,62 @@
+#include <windows.h>
+#include <thread>
+#include "lazy.h"
+#include "skCrypt.h"
 
+typedef NTSTATUS(WINAPI* tNTQSI)(
+	ULONG SystemInformationClass,
+	PVOID SystemInformation,
+	ULONG SystemInformationLength,
+	PULONG ReturnLength
+	);
+
+inline bool hide_thread(HANDLE thread)
+{
+	typedef NTSTATUS(NTAPI* pNtSetInformationThread)(HANDLE, UINT, PVOID, ULONG);
+	NTSTATUS Status;
+
+	pNtSetInformationThread NtSIT = (pNtSetInformationThread)LI_FN(GetProcAddress).forwarded_safe_cached()((LI_FN(GetModuleHandleA).forwarded_safe_cached())(skCrypt("ntdll.dll")), skCrypt("NtSetInformationThread"));
+
+	if (NtSIT == NULL) return false;
+	if (thread == NULL)
+		Status = NtSIT(LI_FN(GetCurrentThread).forwarded_safe_cached(), 0x11, 0, 0);
+	else
+		Status = NtSIT(thread, 0x11, 0, 0);
+
+	if (Status != 0x00000000)
+		return false;
+	else
+		return true;
+}
+
+inline int thread_context() // we look for debuggers hiding in the thread context :sus:
+{
+	int found = false;
+	CONTEXT ctx = { 0 };
+	void* h_thread = LI_FN(GetCurrentThread).forwarded_safe_cached();
+
+	ctx.ContextFlags = CONTEXT_DEBUG_REGISTERS;
+	if (LI_FN(GetThreadContext).forwarded_safe_cached()(h_thread, &ctx))
+	{
+		if ((ctx.Dr0 != 0x00) || (ctx.Dr1 != 0x00) || (ctx.Dr2 != 0x00) || (ctx.Dr3 != 0x00) || (ctx.Dr6 != 0x00) || (ctx.Dr7 != 0x00))
+		{
+			found = true;
+		}
+	}
+
+	return found;
+}
+
+inline int remote_is_present()
+{
+	int debugger_present = false;
+
+	LI_FN(CheckRemoteDebuggerPresent).forwarded_safe_cached()(LI_FN(GetCurrentProcess).forwarded_safe_cached()(), &debugger_present);
+
+	return debugger_present;
+}
+
+int is_debugger_present()
+{
+	return LI_FN(IsDebuggerPresent).forwarded_safe_cached()();
+}
